53042c6610475eb7aa0606a578f80d636edf38e4
Spencer Denton <spencerdent02@gmail.com>
2021-04-29 17:38:15 -0500

    more abstraction

--- src/send_mail.rs
+++ src/send_mail.rs
@@ -1,10 +1,10 @@
-use lettre::{SmtpTransport, Transport};
+use lettre::{SmtpTransport, Transport, smtp::response::Response};
 use lettre::smtp::error::Error;
 
 extern crate lettre;
 extern crate lettre_email;
 
-pub fn _send_email(to: &str, name: &str, domain: &str, username: &str, password: &str) {
+pub fn _send_email(to: &str, name: &str, domain: &str, username: &str, password: &str) -> Result<Response, Error> {
     let email = lettre_email::EmailBuilder::new()
       .to((to, name))
       .from(("micromessager@gmail.com", "Test Email"))
@@ -13,14 +13,13 @@ pub fn _send_email(to: &str, name: &str, domain: &str, username: &str, password:
       .build()
       .unwrap();
 
-    let mut mailer = _make_smtp_transport(domain, username, password).expect("Couldn't verify SMTP");
+    let mut mailer = make_smtp_transport(domain, username, password)?;
     
-    let result = mailer.send(email.into());
-
-    println!("{:?}", result);
+    let result = mailer.send(email.into())?;
+    Ok(result)
 }
 
-fn _make_smtp_transport(domain: &str, username: &str, password: &str) -> Result<SmtpTransport, Error> {
+fn make_smtp_transport(domain: &str, username: &str, password: &str) -> Result<SmtpTransport, Error> {
     let mailer = lettre::SmtpClient::new_simple(domain)?;
     let mailer = mailer.credentials(lettre::smtp::authentication::Credentials::new(username.into(), password.into())).transport();
 
